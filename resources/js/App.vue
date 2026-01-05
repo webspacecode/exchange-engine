0dbeaf2a3071df48751ef8dc951b5dab55286fb7
Archit <archit@192.168.1.5>
2026-01-05 17:46:31 +0530

    Vue setup done

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-10 text-3xl font-bold text-blue-600">
+        Vue 3 ann Tailwind CSS
+    </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
     server: {
         watch: {
